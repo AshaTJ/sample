18a61558f4e42095a7832ce4142b871358f7eadd
Asha TJ <asha.tjaysimha1@gmail.com>
2020-05-11 12:22:03 +0530

    First commit

---
+++ AuthenticationDemo/src/main/java/com/fis/model/Users.java
@@ -0,0 +1,33 @@
+package com.fis.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String username;
+	@JsonIgnore
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
